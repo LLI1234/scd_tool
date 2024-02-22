9fce00c79f2543d460e5415ee9d786c637c3413a
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2024-02-22 09:34:56 -0500

    Initial pages setup

---
+++ lib/pages/match_page.dart
@@ -0,0 +1,119 @@
+import "package:flutter/material.dart";
+
+class MatchPage extends StatefulWidget {
+  const MatchPage({super.key});
+
+  @override
+  State<MatchPage> createState() => _MatchPageState();
+}
+
+class _MatchPageState extends State<MatchPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.90, //Max width
+          child: const SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    Text(
+                      'Selected By Users Like You',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    Text(
+                      'Matches For You',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                CenterCard(),
+                SizedBox(height: 20.0),
+                CenterCard(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CenterCard extends StatelessWidget {
+  const CenterCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CenterDetailsPage()),
+        );
+      },
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          width: double.infinity,
+          height: 110.0,
+        ),
+      ),
+    );
+  }
+}
+
+class CenterDetailsPage extends StatelessWidget {
+  const CenterDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Center Details Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+        child: Text('Center Details Page'),
+      ),
+    );
+  }
+}
