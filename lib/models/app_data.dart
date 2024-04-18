5ce0a4bd9e90df90f2395f8e8fa5486b5ff6b251
Chris Liang <christopherliang.cl@gmail.com>
2024-04-18 17:33:58 -0400

    Changed center data visual

--- lib/models/app_data.dart
+++ lib/models/app_data.dart
@@ -5,12 +5,30 @@ import 'login_data.dart';
 
 class AppData with ChangeNotifier {
   final LoginData loginData;
+  Map<String, dynamic> userInfo = {};
   List<Map<String, dynamic>> savedPhysicians = [];
   List<Map<String, dynamic>> dailySymptoms = [];
   bool error = false;
 
   AppData({required this.loginData});
 
+  Future<void> getUserInfo() async {
+    final cookie = loginData.getCookie();
+    final response = await http.get(
+      Uri.parse('http://localhost:5000/user/current'),
+      headers: {'Cookie': cookie},
+    );
+    //print(response.body);
+    if (response.statusCode == 200) {
+      userInfo =
+          Map<String, dynamic>.from(json.decode(response.body));
+      notifyListeners();
+    } else {
+      error = true;
+      throw Exception('Failed to load center data');
+    }
+  }
+  
   Future<void> getSavedPhysicians() async {
     final cookie = loginData.getCookie();
     final response = await http.get(
