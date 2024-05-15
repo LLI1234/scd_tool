7d079316f6f716ff2d298a6c21fa3d1a83267410
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2024-05-14 22:44:36 -0400

    add: safe area to material app

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,9 @@ class MyApp extends StatelessWidget {
               ? RouteManager.login
               : RouteManager.main,
           onGenerateRoute: RouteManager.generateRoute,
+          builder: (context, child) {
+            return SafeArea(child: child!);
+          },
         );
       },
     );
